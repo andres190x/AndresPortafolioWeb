16b5454057eeafa8327fd0ea7c6d929b54cfa067
andres190x <79940091+andres190x@users.noreply.github.com>
2021-06-14 15:37:02 -0500

    Add files via upload

---
+++ js/codigo.js
@@ -0,0 +1,19 @@
+function contActive() {
+    var selector = document.getElementById("cont-colors");
+    var selector2 = document.getElementById("btn-elemento");
+    selector.classList.toggle("active");
+    selector2.classList.toggle("active");
+}
+function bgmorado() {
+    var selector = document.getElementById("body");
+    selector.classList.toggle("morado");
+}
+function bgamarillo() {
+    var selector = document.getElementById("body");
+    selector.classList.toggle("amarillo");
+}
+function bgazul() {
+    var selector = document.getElementById("body");
+    selector.classList.toggle("azul");
+}
+
